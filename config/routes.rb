01dc7c67386b2221cee88c9331b17f01a5a77753
Your Name <your-email@example.com>
2024-03-16 23:43:18 +0500

    Set up the Project with devise

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :admins
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root  "home#index"
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+  get "admin"=> "admin#index"
+end
+
